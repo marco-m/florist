d75c6f91793dcb2d90fb875eab8f9cdeb3f3f0a0
Marco Molteni <marco.molteni@mailbox.org>
2024-03-01 11:02:07 +0100

    UnzipOne: log debug the archive contents

--- pkg/florist/archive.go
+++ pkg/florist/archive.go
@@ -18,6 +18,17 @@ func UnzipOne(zipPath string, name string, dstPath string) error {
 	}
 	defer rd.Close()
 
+	// We calculate the file list also if the log level is not debug. This is somehow
+	// a waste; we could use the LogValuer interface as explained in
+	// https://pkg.go.dev/log/slog#hdr-Performance_considerations
+	// to optimize. But given that we are doing this in the middle of a disk I/O
+	// operation, I do not think it is worthwhile.
+	files := make([]string, 0, len(rd.File))
+	for _, fi := range rd.File {
+		files = append(files, fi.Name)
+	}
+	log.Debug("archive-contents", "files", files)
+
 	fi, err := rd.Open(name)
 	if err != nil {
 		return fmt.Errorf("UnzipOne: open element: %s", err)
