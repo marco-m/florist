c3e6ebf4e65a443058a0a735b636ef6e1ac6d2c1
Marco Molteni <marco.molteni@mailbox.org>
2024-03-04 17:35:41 +0100

    NetFetch: create directory if missing

--- pkg/florist/net.go
+++ pkg/florist/net.go
@@ -27,7 +27,7 @@ func NetFetch(client *http.Client, url string, hashType Hash, hash string, dstDi
 	log := Log().With("url", url)
 
 	if len(url) == 0 {
-		return "", fmt.Errorf("empty url")
+		return "", fmt.Errorf("NetFetch: empty url")
 	}
 	hasher := sha256.New()
 
@@ -36,7 +36,7 @@ func NetFetch(client *http.Client, url string, hashType Hash, hash string, dstDi
 	fi, err := os.Open(dstPath)
 	if err == nil {
 		if _, err := io.Copy(hasher, fi); err != nil {
-			return "", fmt.Errorf("NetFetch: reading %s: %s", dstPath, err)
+			return "", fmt.Errorf("NetFetch: reading %s: %w", dstPath, err)
 		}
 		have := hex.EncodeToString(hasher.Sum(nil))
 		if have == hash {
@@ -46,21 +46,24 @@ func NetFetch(client *http.Client, url string, hashType Hash, hash string, dstDi
 		}
 	}
 
+	if err := Mkdir(dstDir, User().Username, 0775); err != nil {
+		return "", fmt.Errorf("NetDetch: %w", err)
+	}
 	dst, err := os.Create(dstPath)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("NetDetch: %w", err)
 	}
 	defer dst.Close()
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("NetDetch: %w", err)
 	}
 
 	start := time.Now()
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("NetDetch: %w", err)
 	}
 	defer resp.Body.Close()
 
@@ -73,7 +76,7 @@ func NetFetch(client *http.Client, url string, hashType Hash, hash string, dstDi
 	mw := io.MultiWriter(dst, hasher)
 
 	if _, err := io.Copy(mw, resp.Body); err != nil {
-		return "", fmt.Errorf("NetFetch: saving %s to %s: %s", url, dstPath, err)
+		return "", fmt.Errorf("NetFetch: saving %s to %s: %w", url, dstPath, err)
 	}
 	elapsed := time.Since(start).Round(time.Millisecond)
 	log.Debug("downloaded", "elapsed", elapsed)
