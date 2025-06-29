f80dbdec2e9270abc65f250ed51e7df0a522b57b
Marco Molteni <marco.molteni@mailbox.org>
2025-06-29 21:39:03 +0200

    pulumi: use gotestsum as test runner

--- pulumi/runtests.sh
+++ pulumi/runtests.sh
@@ -2,11 +2,23 @@
 
 set -e
 
+apt-get update > /dev/null
+apt-get install gotestsum > /dev/null
+
 cd /opt/florist/test
 
+#export GOVERSION=1.24
 for t in *.test; do
     echo
     echo ===== $t =====
     #./$t -test.v
-    ./$t
+    #./$t
+    #
+    # https://github.com/gotestyourself/gotestsum/blob/main/.project/docs/running-without-go.md
+    # https://pkg.go.dev/cmd/test2json
+    # -test.v is needed for test2json
+    # -t: add timestamps
+    # -p: set the package name
+    gotestsum --raw-command -- ./test2json -t -p $t ./$t -test.v=test2json
 done
+
