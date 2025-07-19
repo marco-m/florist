526df755c89bd67d807a300be30a993a8772f944
Marco Molteni <marco.molteni@mailbox.org>
2025-07-19 14:30:05 +0200

    exec_test: introduce the TestHelperProcess pattern
    This allows to test florist.CmdRun running a subprocess, capturing the
    subprocess stdout and stderr.

--- pkg/florist/exec_test.go
+++ pkg/florist/exec_test.go
@@ -2,11 +2,14 @@ package florist_test
 
 import (
 	"bytes"
+	"fmt"
 	"log/slog"
+	"os"
 	"os/exec"
 	"strings"
 	"testing"
 
+	"github.com/marco-m/florist/internal"
 	"github.com/marco-m/florist/pkg/florist"
 	"github.com/marco-m/rosina/diff"
 )
@@ -15,7 +18,7 @@ func TestCmdRunBasicSuccess(t *testing.T) {
 	var buf bytes.Buffer
 	log := slog.New(slog.NewTextHandler(&buf, &slog.HandlerOptions{
 		Level:       slog.LevelDebug,
-		ReplaceAttr: RemoveTime,
+		ReplaceAttr: internal.RemoveTime,
 	}))
 
 	err := florist.CmdRun(log, exec.Command("true"))
@@ -32,7 +35,7 @@ func TestCmdRunBasicFailure(t *testing.T) {
 	var buf bytes.Buffer
 	log := slog.New(slog.NewTextHandler(&buf, &slog.HandlerOptions{
 		Level:       slog.LevelDebug,
-		ReplaceAttr: RemoveTime,
+		ReplaceAttr: internal.RemoveTime,
 	}))
 
 	err := florist.CmdRun(log, exec.Command("false"))
@@ -46,10 +49,75 @@ func TestCmdRunBasicFailure(t *testing.T) {
 	}
 }
 
-// RemoveTime removes the "time" attribute from the output of a slog.Logger.
-func RemoveTime(groups []string, a slog.Attr) slog.Attr {
-	if a.Key == slog.TimeKey {
-		return slog.Attr{}
+// This is part of the TestHelperProcess pattern.
+// https://web.archive.org/web/20250204012613/https://npf.io/2015/06/testing-exec-command/
+func fakeExecCommand(command string, args ...string) *exec.Cmd {
+	cs := []string{"-test.run=TestHelperProcess", "--", command}
+	cs = append(cs, args...)
+	cmd := exec.Command(os.Args[0], cs...)
+	cmd.Env = []string{
+		"GO_WANT_HELPER_PROCESS=1",
+		// Avoid diagnostic printed by "go test" to stderr:
+		//   warning: GOCOVERDIR not set, no coverage data emitted
+		"GOCOVERDIR=" + os.TempDir(),
 	}
-	return a
+	return cmd
+}
+
+// This is part of the TestHelperProcess pattern.
+// https://web.archive.org/web/20250204012613/https://npf.io/2015/06/testing-exec-command/
+func TestHelperProcess(*testing.T) {
+	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
+		return
+	}
+
+	// optional: validation logic
+
+	// Simulate a process, run by florist.CmdRun(), that writes to stdout and stderr.
+	fmt.Fprintf(os.Stdout, "line 1 to stdout\n")
+	fmt.Fprintf(os.Stderr, "line 2 to stderr\n")
+	fmt.Fprintf(os.Stdout, "line 3 to stdout\n")
+	fmt.Fprintf(os.Stderr, "line 4 to stderr\n")
+	fmt.Fprintf(os.Stdout, "line 5 to stdout\n")
+	fmt.Fprintf(os.Stderr, "line 6 to stderr\n")
+
+	os.Exit(0)
+}
+
+func TestCmdRunProcWritesToStdoutAndStderr(t *testing.T) {
+	// execCommandFn = fakeExecCommand
+	// defer func() { execCommandFn = exec.Command }()
+
+	var buf bytes.Buffer
+	log := slog.New(slog.NewTextHandler(&buf, &slog.HandlerOptions{
+		Level:       slog.LevelDebug,
+		ReplaceAttr: internal.RemoveTime,
+	}))
+
+	err := florist.CmdRun(log, fakeExecCommand("anything-goes-this-is-a-fake"))
+	if err != nil {
+		t.Errorf("\nhave error: %s\nwant: <no error>", err)
+	}
+
+	// Remove the first line from buffer, because it contains the path to the Go test
+	// executable, which is in a temporary directory. For example:
+	// level=DEBUG msg=cmd-run
+	//   cmd="/var/folders/h2/sz12x.../T/go-build37.../b001/florist.test
+	//   -test.run=TestHelperProcess -- anything-goes-this-is-a-fake"
+	have := buf.String()
+	have = have[strings.Index(have, "\n"):]
+
+	// NOTE the ordering is wrong: first all writes to stdout, then all writes to stderr.
+	// This shows that the implementation is wrong.
+	want := `
+level=DEBUG msg=cmd-run stdout="line 1 to stdout"
+level=DEBUG msg=cmd-run stdout="line 3 to stdout"
+level=DEBUG msg=cmd-run stdout="line 5 to stdout"
+level=DEBUG msg=cmd-run stderr="line 2 to stderr"
+level=DEBUG msg=cmd-run stderr="line 4 to stderr"
+level=DEBUG msg=cmd-run stderr="line 6 to stderr"
+`
+	if text := diff.TextDiff("want", "have", want, have); text != "" {
+		t.Errorf("\nlog mismatch:\n%s", text)
+	}
 }
